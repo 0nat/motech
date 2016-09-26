68e0078017397c50f561e0d254b3859663f4f196
Natalia Kowalik <nkowalik@soldevelo.com>
2016-09-26 15:40:27 +0200

    test

--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    if [ "$DB" = "mysql" ]; then
-        echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
-        mvn clean install -PIT -U
-    elif [ "$DB" = "psql" ]; then
-        mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
-    fi
+if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
+    git clone https://github.com/motech/motech.git ../motech-master -b master --single-branch
+    cd ../motech-master/platform/mds/mds-performance-tests/
+    mvn clean install -Dmds.performance.quantity=10000 -U -PMDSP
+
+    set -e
+
+    PERF_DIR=.
+
+    PERF_RES_DIR=$PERF_DIR/src/test/resources
+
+    TRESHOLDS_FILE=/tmp/tresholds.csv
+
+    touch $TRESHOLDS_FILE
+
+    cat > $TRESHOLDS_FILE <<EOL
+    MdsStressIT,stressTestCreating,40000
+    MdsStressIT,stressTestRetrieval,300
+    MdsStressIT,stressTestUpdating,40000
+    MdsStressIT,stressTestDeleting,250000
+    MdsDiskSpaceUsageIT,testEudeDiskSpaceUsage,25
+    EOL
+
+    $PERF_RES_DIR/performanceCheck.sh -d ~/perf $PERF_DIR/target/performanceTestResult.log $TRESHOLDS_FILE
+    RESULT=$?
+
+    rm -f $TRESHOLDS_FILE
+
+    exit $RESULT
 fi
