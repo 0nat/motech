e51ee43cbebdb72afed2ff776907d92e67cc0e36
Natalia Kowalik <nkowalik@soldevelo.com>
2016-09-26 16:41:23 +0200

    test9

--- run_build.sh
+++ run_build.sh
@@ -2,6 +2,8 @@
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
     git clone https://github.com/motech/motech.git ../motech-master -b master --single-branch
+    mkdir ~/.motech
+    cp ../motech-master/testdata/config-locations.properties ~/.motech/
     cd ../motech-master/platform/mds/mds-performance-tests/
     mvn -Dmds.performance.quantity=10000 clean install -U -PMDSP
 fi
