69d5f122b594bfdc5436416584e03f35650de738
Natalia Kowalik <nkowalik@soldevelo.com>
2016-09-28 09:08:32 +0200

    test 16

--- performance_test.sh
+++ performance_test.sh
@@ -9,6 +9,7 @@ PERF_RES_DIR=$PERF_DIR/src/test/resources
 TRESHOLDS_FILE=/tmp/tresholds.csv
 
 touch $TRESHOLDS_FILE
+touch $PERF_DIR/target/performanceTestResult.log
 
 cat > $TRESHOLDS_FILE <<EOL
 MdsStressIT,stressTestCreating,40000
