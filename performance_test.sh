9d67ffdf42c9895c46b5c17451620269f8731538
Natalia Kowalik <nkowalik@soldevelo.com>
2016-09-28 14:44:02 +0200

    test 20

--- performance_test.sh
+++ performance_test.sh
@@ -20,7 +20,7 @@ MdsStressIT,stressTestDeleting,250000
 MdsDiskSpaceUsageIT,testEudeDiskSpaceUsage,25
 EOL
 
-$PERF_RES_DIR/performanceCheck.sh -d ~/perf $PERF_DIR/target/performanceTestResult.log $TRESHOLDS_FILE
+cat $PERF_RES_DIR/performanceCheck.sh -d ~/perf $PERF_DIR/target/performanceTestResult.log $TRESHOLDS_FILE
 RESULT=$?
 
 rm -f $TRESHOLDS_FILE
